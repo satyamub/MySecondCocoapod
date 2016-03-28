74bc08ec5f7e80abe429177b117b0ca60ab7feab
Arun <Exelanz@Aruns-MacBook-Pro.local>
2016-03-28 16:21:45 +0530

    Initial Commit

---
+++ MySecondCocoapod/MySecondCocoapod.h
@@ -0,0 +1,19 @@
+//
+//  MySecondCocoapod.h
+//  MySecondCocoapod
+//
+//  Created by Arun on 28/03/16.
+//  Copyright © 2016 Muvi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MySecondCocoapod.
+FOUNDATION_EXPORT double MySecondCocoapodVersionNumber;
+
+//! Project version string for MySecondCocoapod.
+FOUNDATION_EXPORT const unsigned char MySecondCocoapodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MySecondCocoapod/PublicHeader.h>
+
+
